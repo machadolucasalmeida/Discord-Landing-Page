7b821330d745fd965da1b6231fa37acea0308586
Lucas Machado <42480063+machadolucasalmeida@users.noreply.github.com>
2023-05-23 20:55:20 -0300

    Upload arquivos do projeto

---
+++ reveal.js
@@ -0,0 +1,7 @@
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.reveal-animation-01', { delay: 200 });
+sr.reveal('.reveal-animation-02', { delay: 200 });
+sr.reveal('.reveal-animation-03', { delay: 200 });
+sr.reveal('.reveal-animation-04', { delay: 200 });
